80bb6e38a2ab1cb3e828a0ae6a503a92ad469a37
Deepanshu Gupta <deepanshu@google.com>
2015-07-23 20:54:59 -0700

    Handle new density constant - 360dpi
    Which is part of API 23
    
    Bug: 19528291
    Change-Id: Iaad34bc21ef8cdf6b0c933cb97e4899ce9ec5188
    (cherry picked from commit 0446853d5b10462834a86f8594cd909b9028f211)
    (cherry picked from commit 22d93acd44784a4c5d18b5bc6e24dc5059478e50)

--- android/src/com/android/tools/idea/avdmanager/ConfigureAvdOptionsStep.java
+++ android/src/com/android/tools/idea/avdmanager/ConfigureAvdOptionsStep.java
@@ -971,6 +971,7 @@ public class ConfigureAvdOptionsStep extends DynamicWizardStepWithDescription {
         case TV:
         case HIGH:
         case DPI_280:
+        case DPI_360:
           vmHeapSize = 64;
           break;
         case XHIGH:
@@ -994,6 +995,7 @@ public class ConfigureAvdOptionsStep extends DynamicWizardStepWithDescription {
         case TV:
         case HIGH:
         case DPI_280:
+        case DPI_360:
           vmHeapSize = 32;
           break;
         case XHIGH:
